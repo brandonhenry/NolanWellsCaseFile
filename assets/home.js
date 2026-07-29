8b37ae034c416398ba7c60af2849ffff32ef5aa5
Brandon Henry <itsbhenry@gmail.com>
2026-07-28 20:44:21 -0500

    Remove visible Leaflet branding

--- assets/home.js
+++ assets/home.js
@@ -171,6 +171,7 @@
       touchZoom: false,
       keyboard: false
     }).setView([30.30, -88.70], 11);
+    map.attributionControl.setPrefix(false);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
